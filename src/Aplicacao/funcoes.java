bb5d899748052fa88a92dcd56023f0963981b62e
Joao Borges10 <126673387+Joaoborges10@users.noreply.github.com>
2023-06-08 21:03:13 -0300

    enfim samplix participou

--- src/Aplicacao/funcoes.java
+++ src/Aplicacao/funcoes.java
@@ -1,5 +1,9 @@
 package Aplicacao;
-
+import java.util.Scanner;
+import Enumeracao.TipoCliente;
+import entidades.ClienteFisico;
+import entidades.Endereco;
+import entidades.Telefone;
 public class funcoes {
 
     public void ApresentaMenu(){
@@ -22,4 +26,86 @@ public class funcoes {
 				
 	}
     
+	
+
+
+    public ClienteFisico cadastrarClienteFisico() {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Cadastro de Cliente Físico");
+        
+        // Solicitar informações do cliente
+        System.out.print("CPF: ");
+        String cpf = scanner.nextLine();
+        
+        System.out.print("Nome: ");
+        String nome = scanner.nextLine();
+        
+        // Criar uma instância de ClienteFisico
+        ClienteFisico cliente = new ClienteFisico();
+        
+        // Definir os valores do cliente
+        cliente.setCPF(cpf);
+        cliente.setNome(nome);
+        
+        // Chamar as funções de cadastro e atribuir os valores retornados
+        Telefone telefone = cadastrarTelefone();
+        cliente.setDDD(telefone.getDDD());
+        cliente.setTelefone(telefone.getNumero());
+        
+        Endereco endereco = cadastrarEndereco();
+        cliente.setNumeroEndereco(endereco.getNumero());
+        cliente.setLogradouro(endereco.getLogradouro());
+        
+        scanner.close();
+        
+        // Retornar o objeto ClienteFisico preenchido
+        return cliente;
+    }
+    
+    public Telefone cadastrarTelefone() {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Cadastro de Telefone");
+        
+        // Solicitar informações de telefone
+        System.out.print("DDD: ");
+        int ddd = scanner.nextInt();
+        scanner.nextLine(); // Consumir a nova linha
+        
+        System.out.print("Número: ");
+        String numero = scanner.nextLine();
+        
+        scanner.close();
+        
+        // Criar uma instância de Telefone
+        Telefone telefone = new Telefone(ddd, numero);
+        
+        // Retornar o objeto Telefone preenchido
+        return telefone;
+    }
+    
+    public Endereco cadastrarEndereco() {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Cadastro de Endereço");
+        
+        // Solicitar informações de endereço
+        System.out.print("Número do Endereço: ");
+        int numeroEndereco = scanner.nextInt();
+        scanner.nextLine(); // Consumir a nova linha
+        
+        System.out.print("Logradouro: ");
+        String logradouro = scanner.nextLine();
+        
+        scanner.close();
+        
+        // Criar uma instância de Endereco
+        Endereco endereco = new Endereco(numeroEndereco, logradouro);
+        
+        // Retornar o objeto Endereco preenchido
+        return endereco;
+    }
 }
+
+
